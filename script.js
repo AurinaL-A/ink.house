c6cb317cdc8d81c84b07024163fc471ac165a891
AurinaL-A <120450448+AurinaL-A@users.noreply.github.com>
2024-07-20 02:59:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ button.addEventListener('click', function() {
   this.classList.toggle('btn-alt-color');
 
 }, false);
+
+const francBtn = document.querySelector('.franc_btn');
+const germanBtn = document.querySelector('.german_btn');
+const englandBtn = document.querySelector('.england_btn');
+const francCards = document.querySelectorAll('.franc');
+const germanCards = document.querySelectorAll('.german');
+const englandCards = document.querySelectorAll('.england');
+
+francBtn.addEventListener('click', () => {
+  francCards.forEach(card => card.style.display = 'block');
+  germanCards.forEach(card => card.style.display = 'none');
+  englandCards.forEach(card => card.style.display = 'none');
+});
+
+germanBtn.addEventListener('click', () => {
+  francCards.forEach(card => card.style.display = 'none');
+  germanCards.forEach(card => card.style.display = 'block');
+  englandCards.forEach(card => card.style.display = 'none');
+});
+
+englandBtn.addEventListener('click', () => {
+  francCards.forEach(card => card.style.display = 'none');
+  germanCards.forEach(card => card.style.display = 'none');
+  englandCards.forEach(card => card.style.display = 'block');
+});
